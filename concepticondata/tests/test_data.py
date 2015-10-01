8c423a57e726be1424a8169b2f14672abd3cfcc3
LinguList <mattis.list@uni-marburg.de>
2015-10-01 09:11:31 +0200

    added warnings message to test script

--- concepticondata/tests/test_data.py
+++ concepticondata/tests/test_data.py
@@ -1,6 +1,8 @@
 import os
 import re
 import io
+import glob
+import warnings
 
 from concepticondata.util import data_path, tsv_items, split_ids
 
@@ -15,6 +17,11 @@ def error(msg, name, line=''):
     if line:
         line = ':%s' % line
     print('ERROR:%s%s: %s' % (name, line, msg))
+
+def warning(msg, name, line=''):
+    if line:
+        line = ':%s' % line
+    warnings.warn('WARNING:%s%s: %s' % (name, line, msg), Warning)
 
 
 def read_tsv(path, unique='ID'):
@@ -31,6 +38,12 @@ def read_tsv(path, unique='ID'):
             uniquevalues.add(row[unique])
         rows.append((line, row))
     return rows
+
+def read_sources(path):
+
+    infiles = glob.glob(os.path.join(path, '*.pdf'))
+
+    return [os.path.split(f)[-1][:-4] for f in infiles]
 
 
 def test():
@@ -49,14 +62,29 @@ def test():
     #
     # Make sure only records in the BibTeX file references.bib are referenced by
     # concept lists.
-    #
     clmd = data_path('conceptlists.tsv')
     clids = []
+    visited = []
     for i, cl in read_tsv(clmd):
         clids.append(cl['ID'])
         for ref in split_ids(cl['REFS']):
-            if ref not in refs:
+            if ref not in refs and ref not in visited:
                 error('unknown bibtex record "%s" referenced' % ref, clmd, i)
+                visited += [ref]
+
+    #
+    # make also sure that all sources are accompanied as pdf, but only write a
+    # warning if this is not the case
+    #
+    pdfs = read_sources('sources')
+    short_path = os.path.split(clmd)[1]
+    for i, cl in read_tsv(clmd):
+        for ref in split_ids(cl['REFS']):
+            if ref not in pdfs:
+
+                warning('no source found for bibtex record "%s" ' % ref,
+                        short_path, i)
+
 
     ref_cols = {
         'CONCEPTICON_ID': set(cs[1]['ID'] for cs in concepticon),
