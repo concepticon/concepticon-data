1bdcaf6182c3ba37fee7271fe621ec6f19c084b2
xrotwang <xrotwang@googlemail.com>
2017-07-18 08:11:03 +0200

    bumped version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyconcepticon',
-    version='1.1.1',
+    version='1.2.0',
     description='programmatic access to concepticon-data',
     long_description='',
     classifiers=[
