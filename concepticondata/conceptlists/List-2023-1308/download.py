d8e29b9484bf35fe6b265656d055fd34aa94da67
Robert Forkel <xrotwang@googlemail.com>
2023-12-20 16:58:22 +0100

    refactored network handling for 4 concept lists.

--- concepticondata/conceptlists/List-2023-1308/download.py
+++ concepticondata/conceptlists/List-2023-1308/download.py
@@ -1,14 +1,10 @@
-from urllib.request import urlopen
+from urllib.request import urlretrieve
 import zipfile
 
-with urlopen(
-        "https://github.com/lingpy/pacs/raw/main/examples/colexifications-graphs.zip"
-        ) as f:
-    data = f.read()
-with open("raw/graphs.zip", "wb") as f:
-    f.write(data)
+
+urlretrieve(
+    "https://github.com/lingpy/pacs/raw/main/examples/colexifications-graphs.zip",
+    "raw/graphs.zip")
 
 with zipfile.ZipFile("raw/graphs.zip", "r") as obj:
     obj.extractall(path="raw")
-
-
--- concepticondata/conceptlists/Zalizniak-2020-2590/convert.py
+++ concepticondata/conceptlists/Zalizniak-2020-2590/convert.py
@@ -1,58 +1,46 @@
-from csvw.dsv import UnicodeDictReader, UnicodeWriter
-from pyconcepticon import Concepticon, models
-import json
-from collections import defaultdict
+import pathlib
+import collections
+
+from pyconcepticon import models
+from pyconcepticon.util import ConceptlistWithNetworksWriter
 
 dss = models.Conceptlist.from_file("raw/Zalizniak-2020-2590.tsv")
 
-num2id, num2name = {}, {}
-for concept in dss.concepts.values():
-    num2id[concept.number] = concept.id
-    num2name[concept.number] = concept.english
+num2id = {concept.number: concept.id for concept in dss.concepts.values()}
+num2name = {concept.number: concept.english for concept in dss.concepts.values()}
 
-table = []
-
-graph = {}
-for concept in dss.concepts.values():
-    links = concept.attributes["links"].split()
-    dirs = concept.attributes["directions"].split()
-    shifts = concept.attributes["urls"].split()
-    weights = concept.attributes["weights"].split()
+with ConceptlistWithNetworksWriter(pathlib.Path(__file__).parent.name) as table:
+    graph = {}
+    for concept in dss.concepts.values():
+        links = concept.attributes["links"].split()
+        dirs = concept.attributes["directions"].split()
+        shifts = concept.attributes["urls"].split()
+        weights = concept.attributes["weights"].split()
     
-    polysemy, sources, targets = [], [], []
-    for link, direction, shift, weight in zip(links, dirs, shifts, weights):
-        if direction in ['—', '↔']:
-            polysemy += [{
-                "ID": num2id[link],
-                "NAME": num2name[link],
-                "Weight": weight,
-                "Shifts": "https://datsemshift.ru/" + shift
+        polysemy, sources, targets = [], [], []
+        for link, direction, shift, weight in zip(links, dirs, shifts, weights):
+            if direction in ['—', '↔']:
+                polysemy += [{
+                    "ID": num2id[link],
+                    "NAME": num2name[link],
+                    "Weight": weight,
+                    "Shifts": "https://datsemshift.ru/" + shift
                 }]
-        elif direction == "→":
-            targets += [{
-                "ID": num2id[link],
-                "NAME": num2name[link],
-                "Weight": weight,
-                "Shifts": "https://datsemshift.ru/" + shift
+            elif direction == "→":
+                targets += [{
+                    "ID": num2id[link],
+                    "NAME": num2name[link],
+                    "Weight": weight,
+                    "Shifts": "https://datsemshift.ru/" + shift
                 }]
-    row = [
-            concept.id,
-            concept.number,
-            concept.english,
-            concept.concepticon_id,
-            concept.concepticon_gloss,
-            concept.attributes["rank"],
-            concept.attributes["degree"],
-            concept.attributes["weighted_degree"],
-            json.dumps(polysemy),
-            json.dumps(targets)
-            ]
-    table += [row]
-
-with UnicodeWriter("Zalizniak-2020-2590.tsv", delimiter="\t") as writer:
-    writer.writerow([
-        "ID", "NUMBER", "ENGLISH", "CONCEPTICON_ID", "CONCEPTICON_GLOSS", "RANK", "DEGREE", "WEIGHTED_DEGREE",
-        "LINKED_CONCEPTS", "TARGET_CONCEPTS"])
-    for row in table:
-        writer.writerow(row)
-
+        table.append(collections.OrderedDict([
+            ('NUMBER', concept.number),
+            ('ENGLISH', concept.english),
+            ('CONCEPTICON_ID', concept.concepticon_id),
+            ('CONCEPTICON_GLOSS', concept.concepticon_gloss),
+            ('RANK', concept.attributes["rank"]),
+            ('DEGREE', concept.attributes["degree"]),
+            ('WEIGHTED_DEGREE', concept.attributes["weighted_degree"]),
+            ('LINKED_CONCEPTS', polysemy),
+            ('TARGET_CONCEPTS', targets),
+        ]))
