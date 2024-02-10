3834f5f59a45c10269dbe3ebb2f510a1bb959713
lingulist <mattis.list@lingpy.org>
2024-02-10 06:50:15 +0100

    adjust to new pyconcepticon

--- concepticondata/conceptlists/Zalizniak-2024-4583/convert.py
+++ concepticondata/conceptlists/Zalizniak-2024-4583/convert.py
@@ -1,33 +1,40 @@
 from pycldf import Dataset
 from csvw.dsv import UnicodeWriter, UnicodeDictReader
 import json
+from pyconcepticon.util import ConceptlistWithNetworksWriter
+import pathlib
+import collections
 
 
 with UnicodeDictReader("raw/parameters.csv") as reader:
     data = [row for row in reader]
 
-table = [["ID", "NUMBER", "ENGLISH", "CONCEPTICON_ID", "CONCEPTICON_GLOSS",
-          "LINKED_CONCEPTS", "TARGET_CONCEPTS", "SHIFTS", "DEFINITION",
-          "ALIAS", "DOMAIN"]]
 id2id = {}
 for row in data:
     id2id[row["ID"]] = "Salizniak-2024-4583-" + row["Number"]
 
-for row in data:
-    links = []
-    for t in json.loads(row["Linked_Concepts"]):
-        t["ID"] = id2id[t["ID"]]
-        links += [t]
-    targets = []
-    for t in json.loads(row["Target_Concepts"]):
-        t["ID"] = id2id[t["ID"]]
-        targets += [t]
+with ConceptlistWithNetworksWriter(
+        pathlib.Path(__file__).parent.name) as table:
 
-    table += [[id2id[row["ID"]], row["Number"], row["Name"],
-               row["Concepticon_ID"], row["Concepticon_Gloss"],
-               json.dumps(links), json.dumps(targets), row["Shifts"],
-               row["Definition"], row["Alias"], row["Domain"]]]
-    
-with UnicodeWriter("Zalizniak-2024-4583.tsv", delimiter="\t") as writer:
-    for row in table:
-        writer.writerow(row)
+    for row in data:
+        new_row = collections.OrderedDict([
+            ("NUMBER", row["Number"]),
+            ("ENGLISH", row["Name"]),
+            ("CONCEPTICON_ID", row["Concepticon_ID"]),
+            ("CONCEPTICON_GLOSS", row["Concepticon_Gloss"]),
+            ("SHIFTS", row["Shifts"]),
+            ("DEFINITION", row["Definition"]),
+            ("ALIAS", row["Alias"]),
+            ("DOMAIN", row["Domain"])])
+        links = []
+        for t in json.loads(row["Linked_Concepts"]):
+            t["ID"] = id2id[t["ID"]]
+            links += [t]
+        targets = []
+        for t in json.loads(row["Target_Concepts"]):
+            t["ID"] = id2id[t["ID"]]
+            targets += [t]
+        new_row["TARGET_CONCEPTS"] = targets
+        new_row["LINKED_CONCEPTS"] = links
+        table.append(new_row)
+
